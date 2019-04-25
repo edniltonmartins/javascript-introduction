d4bc940104ea89274c9c7c819f8d2eea22fd1199
ednilton.silva-at-514001089657 <ednilton.silva@cipa.com.br>
2019-04-25 01:32:10 -0300

    First sample

---
+++ exemplo1.js
@@ -0,0 +1,25 @@
+// c-like é o javascript
+
+const valor = 10;
+
+// Dois iguais tenta parsear pra chegar no tipo do valor
+// Três iguais tenta verificar o tipo e o valor
+if (valor===10){
+    //valor = 20
+}
+
+let valor2 = 10;
+
+valor2+=10
+
+console.log( `imprimindo a constante valor: ${valor}`);
+console.log( `imprimindo a variável criada com let ${valor2}`);
+
+const objeto = { chave1: "valor1", chave2: "valor2"};
+console.log(objeto)
+
+const str = "Ednilton Maritns - DevPleno";
+
+const soma = function(a, b){
+    return a + b
+}
